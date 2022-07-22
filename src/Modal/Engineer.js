6b3dc27803ff3839f23089c04dae3479d7b82a82
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-21 21:47:48 -0500

    removed eslint fixed forms modal still broke as heck

--- src/Modal/Engineer.js
+++ src/Modal/Engineer.js
@@ -4,30 +4,21 @@ export default function Engineer() {
     return (
 <>
 <div>
-    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Employee Name</label>
+    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Engineer Name</label>
     <input type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Andrew Edwards" required="" />
 </div>
     <div>
-        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Employee Email</label>
-        <input type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="AndrewEdwards@me.com" required="" />
+        <label for="id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Engineer ID</label>
+        <input type="id" name="id" id="id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="1234" required="" />
     </div>
         <div>
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="" />
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Engineer Email</label>
+            <input type="email" name="email" id="email" placeholder="Engineer@Company.com" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="" />
         </div>
-            <div class="flex justify-between">
-                <div class="flex items-start">
-                    <div class="flex items-center h-5">
-                        <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="" />
-                    </div>
-                        <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                </div>
-                        <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-            </div>
-                        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                            Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
-</div>
+        <div>
+            <label for="github" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Engineer GitHub</label>
+            <input type="github" name="github" id="github" placeholder="Engineer@Company.com" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="" />
+        </div>
 </>
 )
 }
