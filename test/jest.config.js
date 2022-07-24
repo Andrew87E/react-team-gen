cc9247f29aac0694d6616a77c2ac94841fda8c0c
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-24 11:33:53 -0500

    cleaned up file structure, small project has become massive..

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,5 +1,5 @@
 const { pathsToModuleNameMapper } = require("ts-jest/utils");
-const { compilerOptions } = require("../tsconfig.json");
+const { compilerOptions } = require("../frontend/tsconfig.json");
 
 const paths = compilerOptions.paths ? compilerOptions.paths : {};
 
