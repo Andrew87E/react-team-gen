d4960d2fb2b05a931f1daa96af92377e5b5d3c2f
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-24 11:09:17 -0500

    switched to types frontend written

---
+++ src/components/Modal/modal-forms/Manager.tsx
@@ -0,0 +1,30 @@
+
+export default function Manager() {
+
+    return (
+<>
+<div>
+    <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Manager Name</label>
+    <input type="name" name="name" id="name" className="w-100 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Andrew Edwards" required="" />
+</div>
+    <div>
+        <label for="id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Manager ID</label>
+        <input type="id" name="id" id="id" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="1234" required="" />
+    </div>
+        <div>
+            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Manager Email</label>
+            <input type="email" name="email" id="email" placeholder="Manager@Company.com" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="" />
+        </div>
+        <div>
+            <label for="office-number" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Manager Office Number</label>
+            <input type="office-number" name="office-number" id="office-number" placeholder="Manager@Company.com" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="" />
+        </div>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                                        onClick={handleSubmit()}
+                                        >
+                                            Submit
+                                        </button>
+</>
+)
+}
---
+++ utils/Engineer.js
@@ -0,0 +1,18 @@
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+  constructor(name, id, email, gitHub) {
+    super(name, id, email);
+    this.gitHub = gitHub;
+  }
+
+  getRole() {
+    return "Engineer";
+  }
+
+  getGithub() {
+    return `HTTPS://github.com/${this.gitHub}`; //change this var later
+  }
+}
+
+module.exports = Engineer;
