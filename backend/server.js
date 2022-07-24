917c44c2206547341c58d0ec8046bce0780fd669
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-24 14:16:42 -0500

    MONGO DB WORKSSSSSSSSS

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
-
+const colors = require('colors')
+const connectDB = require('./config/db')
 const app = express();
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
