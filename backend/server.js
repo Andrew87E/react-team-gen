12185561aefc57a7551ad0a20c0b12a39be00f7d
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-29 13:03:36 -0500

    heroku restructuring

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,14 @@ const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const colors = require('colors')
 const app = express();
-const connectDB = require('./backend/config/db')
+const connectDB = require('./config/db')
 const path = require('path')
 
 const publicPath = path.join(__dirname, 'frontend', 'dist');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use('/api/employees', require('./backend/routes/employeeRoutes'));
+app.use('/api/employees', require('./routes/employeeRoutes'));
 app.use(express.static(publicPath))
 
 app.get('/', (req, res) => {
