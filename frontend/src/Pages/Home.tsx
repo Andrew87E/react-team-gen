a7be470cf3dc95dd9c71c72c739294d4dea4d288
andrew87e <andrew87e@gmail.com>
2022-07-29 00:25:19 -0500

    added login base and routes

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,12 +1,11 @@
 import Card from "../components/cards/Card";
 import { JumbotronPage } from "../components/jumbotron/Jumbotron";
-import Login from "../components/login/login";
 
-export default function Home(){
-return(
-  <>
-  <JumbotronPage />
-  <Login />
-  </>
-)
+export default function Home() {
+  return (
+    <>
+      <JumbotronPage />
+      <Card />
+    </>
+  );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,15 +5,11 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import LoggedIn from "./Pages/LoggedIn";
-import { Navbar } from "./components/navbar/navbar";
-import { JumbotronPage } from "./components/jumbotron/Jumbotron";
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Navbar />
-    <JumbotronPage />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<LoggedIn />} />
